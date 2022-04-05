ef6c3c2cbf809ecc2c264cea1db96a24c8e55d1d
ishikakohli <95765422+ishikakohli@users.noreply.github.com>
2022-04-05 15:00:04 -0400

    Update button_code.py

--- button_code.py
+++ button_code.py
@@ -12,3 +12,20 @@ btn.pack(side = 'bottom')
 root.mainloop()
 
 ##from geeksforgeeks
+
+#How to add a button that prints something
+
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+
+
+def button_clicked():
+    print('Patient has died')
+
+
+button = ttk.Button(root, text='Increase meds', command=button_clicked)
+button.pack()
+
+root.mainloop()
